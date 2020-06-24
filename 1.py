1ed9433a6ae1efc9a956d4db4570d4e081d768d3
adeelkhan-ak <ak140175@gmail.com>
2020-06-24 08:04:30 -0700

    extra changes

--- 1.py
+++ 1.py
@@ -1,3 +1,22 @@
 import requests
-request = requests.get('http://api.open-notify.org')
-print(request.text)
+
+#request = requests.get('http://api.open-notify.org')
+#print(request.text)
+#print(request.status_code)
+#request2 = requests.get('http://api.open-notify.org/fake-endpoint')
+#print(request2.status_code)
+#people = requests.get('http://api.open-notify.org/astros.json')
+#print(people.text)
+#people_json  = people.json()
+#print(people_json)
+#To print the number of people in space
+#print("Number of people in space:",people_json['number'])
+#To print the names of people in space using a for loop
+#for p in people_json['people']:
+#   print(p['name'])
+
+parameter = {"rel_rhy":"jingle"}
+request = requests.get('https://api.datamuse.com/words',parameter)
+rhyme_json = request.json()
+for i in rhyme_json[0:3]:
+ print(i['word'])
